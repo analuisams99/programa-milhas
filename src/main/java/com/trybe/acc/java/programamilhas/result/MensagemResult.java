1168583473c7cb856f880132d555299e55ae901e
Ana Luisa <ana.luisa.isa@hotmail.com>
2023-04-05 19:21:04 -0300

    feat(result): Implementação da classe MensagemResult, com seus Getters e Setters

---
+++ src/main/java/com/trybe/acc/java/programamilhas/result/MensagemResult.java
@@ -0,0 +1,19 @@
+package com.trybe.acc.java.programamilhas.result;
+
+/**Classe MensagemResult.*/
+public class MensagemResult {
+
+  private String mensagem;
+
+  public MensagemResult(String mensagem) {
+    this.mensagem = mensagem;
+  }
+
+  public String getMensagem() {
+    return mensagem;
+  }
+
+  public void setMensagem(String mensagem) {
+    this.mensagem = mensagem;
+  }
+}
