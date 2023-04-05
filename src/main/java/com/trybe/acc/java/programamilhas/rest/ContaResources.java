2b1423a5089ac8ed82e9d85596f85150930a1411
Ana Luisa <ana.luisa.isa@hotmail.com>
2023-04-05 20:31:43 -0300

    feat(Conta/Resource): Implementação do método buscaExtrato da classe ContaResource

---
+++ src/main/java/com/trybe/acc/java/programamilhas/rest/ContaResources.java
@@ -0,0 +1,34 @@
+package com.trybe.acc.java.programamilhas.rest;
+
+import com.trybe.acc.java.programamilhas.exception.AcessoNaoAutorizadoException;
+import com.trybe.acc.java.programamilhas.model.Lancamento;
+import com.trybe.acc.java.programamilhas.service.ContaService;
+import com.trybe.acc.java.programamilhas.util.TokenUtil;
+import java.util.List;
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.QueryParam;
+
+/**Classe ContaResources.*/
+@Path("/conta")
+@ApplicationScoped
+public class ContaResources {
+
+  @Inject
+  ContaService service;
+
+  @Inject
+  TokenUtil tokenUtil;
+
+  @GET
+  @Path("/extrato")
+  public List<Lancamento> buscaExtrato(@QueryParam("token") String token)
+      throws AcessoNaoAutorizadoException {
+    Integer idUsuario = tokenUtil.obterIdUsuario(token);
+    return service.buscaExtrato(idUsuario);
+  }
+
+
+}
