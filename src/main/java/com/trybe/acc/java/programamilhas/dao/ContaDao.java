407f79ae421794b38107cbb28c4597813bf3ace3
Ana Luisa <ana.luisa.isa@hotmail.com>
2023-04-05 20:30:02 -0300

    feat(Conta/DAO): Implementação do método buscaSaldo da classe ContaDao

--- src/main/java/com/trybe/acc/java/programamilhas/dao/ContaDao.java
+++ src/main/java/com/trybe/acc/java/programamilhas/dao/ContaDao.java
@@ -1,6 +1,7 @@
 package com.trybe.acc.java.programamilhas.dao;
 
 import com.trybe.acc.java.programamilhas.model.Lancamento;
+import com.trybe.acc.java.programamilhas.result.SaldoResult;
 import java.util.List;
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
@@ -20,6 +21,24 @@ public class ContaDao {
     return entityManager.createQuery(hql, Lancamento.class)
         .setParameter("idUsuario", idUsuario)
         .getResultList();
+  }
+
+  /**Método buscaSaldo.*/
+  public SaldoResult buscaSaldo(Integer idUsuario) {
+    String hql = "SELECT l.usuario.id, l.usuario.login, "
+        + "SUM(l.valor) AS saldo "
+        + "FROM Lancamento l "
+        + "WHERE l.usuario.id = :idUsuario ";
+
+    Object[] row = entityManager.createQuery(hql, Object[].class)
+        .setParameter("idUsuario", idUsuario)
+        .getSingleResult();
+
+    Integer userId = (Integer) row[0];
+    String login = (String) row[1];
+    Long saldo = (Long) row[2];
+
+    return new SaldoResult(userId, login, saldo.intValue());
   }
 
 }
