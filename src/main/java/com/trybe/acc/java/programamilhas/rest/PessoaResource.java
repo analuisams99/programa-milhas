574c74364af37ee4754f529b9d73c65d8cd89358
Ana Luisa <ana.luisa.isa@hotmail.com>
2023-04-05 19:50:33 -0300

    feat(Pessoa/Resource): Implementação do método deletePessoa da classe PessoaResource, no caminho /pessoa

--- src/main/java/com/trybe/acc/java/programamilhas/rest/PessoaResource.java
+++ src/main/java/com/trybe/acc/java/programamilhas/rest/PessoaResource.java
@@ -1,14 +1,17 @@
 package com.trybe.acc.java.programamilhas.rest;
 
 import com.trybe.acc.java.programamilhas.dto.LoginDto;
+import com.trybe.acc.java.programamilhas.exception.AcessoNaoAutorizadoException;
 import com.trybe.acc.java.programamilhas.result.MensagemResult;
 import com.trybe.acc.java.programamilhas.service.PessoaService;
 import java.security.NoSuchAlgorithmException;
 import java.security.spec.InvalidKeySpecException;
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.QueryParam;
 
 /**
  * Classe responsável por expor os serviços de pessoa.
@@ -25,6 +28,12 @@ public class PessoaResource {
   public MensagemResult addPessoa(LoginDto login)
       throws InvalidKeySpecException, NoSuchAlgorithmException  {
     return service.salvar(login);
+  }
+
+  @DELETE
+  public MensagemResult deletePessoa(@QueryParam("token") String token)
+      throws AcessoNaoAutorizadoException {
+    return service.deletar(token);
   }
 
 }
