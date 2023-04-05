177e6831733ed9f55703165f560e8a018726da85
Ana Luisa <ana.luisa.isa@hotmail.com>
2023-04-05 20:23:14 -0300

    feat(Dominio/Service): Implementação do método getTiposLancamento da classe DominioService

--- src/main/java/com/trybe/acc/java/programamilhas/service/DominioService.java
+++ src/main/java/com/trybe/acc/java/programamilhas/service/DominioService.java
@@ -3,6 +3,7 @@ package com.trybe.acc.java.programamilhas.service;
 import com.trybe.acc.java.programamilhas.dao.DominioDao;
 import com.trybe.acc.java.programamilhas.model.Parceiro;
 import com.trybe.acc.java.programamilhas.model.Produto;
+import com.trybe.acc.java.programamilhas.model.TipoLancamento;
 import java.util.List;
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
@@ -20,6 +21,10 @@ public class DominioService {
 
   public List<Produto> getProdutos() {
     return dominioDao.getProdutos();
+  }
+
+  public List<TipoLancamento> getTiposLancamento() {
+    return dominioDao.getTiposLancamento();
   }
 
 }
