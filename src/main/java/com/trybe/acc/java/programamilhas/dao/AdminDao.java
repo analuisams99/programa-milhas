c1a8dd75f7500fdd270668d270cdd0c2ccc13612
Ana Luisa <ana.luisa.isa@hotmail.com>
2023-04-05 20:02:17 -0300

    feat(Admin/DAO): Implementação do método adicionarCredito da classe AdminDao, injetando a dependencia EntityManager

---
+++ src/main/java/com/trybe/acc/java/programamilhas/dao/AdminDao.java
@@ -0,0 +1,22 @@
+package com.trybe.acc.java.programamilhas.dao;
+
+import com.trybe.acc.java.programamilhas.model.Lancamento;
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.transaction.Transactional;
+
+/**Classe AdminDao - responsável pela persistência dos dados para o administrador.*/
+@ApplicationScoped
+public class AdminDao {
+
+  @Inject
+  EntityManager entityManager;
+
+  @Transactional
+  public void adicionaCredito(Lancamento lancamento) {
+    entityManager.persist(lancamento);
+    return;
+  }
+
+}
