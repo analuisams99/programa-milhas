6a7eea39f6ce72a51c36667298aadcd223dfbfb9
Ana Luisa <ana.luisa.isa@hotmail.com>
2023-04-05 20:07:21 -0300

    feat(Admin/Service): Implementação do método efetuarResgate da classe AdminService

--- src/main/java/com/trybe/acc/java/programamilhas/service/AdminService.java
+++ src/main/java/com/trybe/acc/java/programamilhas/service/AdminService.java
@@ -5,8 +5,10 @@ import com.trybe.acc.java.programamilhas.dao.PessoaDao;
 import com.trybe.acc.java.programamilhas.dto.LancamentoDto;
 import com.trybe.acc.java.programamilhas.model.Lancamento;
 import com.trybe.acc.java.programamilhas.model.Pessoa;
+import com.trybe.acc.java.programamilhas.model.TipoLancamento;
 import com.trybe.acc.java.programamilhas.result.MensagemResult;
 import com.trybe.acc.java.programamilhas.util.LancamentoUtil;
+import java.time.LocalDate;
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
 
@@ -39,6 +41,26 @@ public class AdminService {
     adminDao.adicionaCredito(lancamento);
 
     return new MensagemResult("Crédito adicionado com sucesso!");
+  }
+
+  /**Método responsável por efetuar um resgate para um usuário.*/
+  public MensagemResult efetuaResgate(LancamentoDto lancamentoDto) {
+    Pessoa pessoa = pessoaDao.buscarPorNome(lancamentoDto.getUsuario());
+
+    TipoLancamento tipoLancamento = new TipoLancamento();
+    tipoLancamento.setId(lancamentoDto.getIdTipoLancamento());
+    tipoLancamento.setDescricao(lancamentoDto.getDescricao());
+
+    Lancamento lancamento = new Lancamento();
+    lancamento.setUsuario(pessoa);
+    lancamento.setTipoLancamento(tipoLancamento);
+    lancamento.setValor(lancamentoDto.getValor() * -1);
+    lancamento.setDescricao(lancamentoDto.getDescricao());
+    lancamento.setData(LocalDate.now());
+
+    adminDao.efetuaResgate(lancamento);
+
+    return new MensagemResult("Resgate efetuado com sucesso!");
   }
 
 }
