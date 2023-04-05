3e8906a513050740aa57e57a20628c627cf7c5e8
Ana Luisa <ana.luisa.isa@hotmail.com>
2023-04-05 20:24:36 -0300

    feat(Dominio/Resource): Implementação do método getTipoLancamento da classe DominioResource

--- src/main/java/com/trybe/acc/java/programamilhas/rest/DominioResource.java
+++ src/main/java/com/trybe/acc/java/programamilhas/rest/DominioResource.java
@@ -2,6 +2,7 @@ package com.trybe.acc.java.programamilhas.rest;
 
 import com.trybe.acc.java.programamilhas.model.Parceiro;
 import com.trybe.acc.java.programamilhas.model.Produto;
+import com.trybe.acc.java.programamilhas.model.TipoLancamento;
 import com.trybe.acc.java.programamilhas.service.DominioService;
 import java.util.List;
 import javax.enterprise.context.ApplicationScoped;
@@ -27,6 +28,12 @@ public class DominioResource {
   @Path("/produto")
   public List<Produto> getProdutos() {
     return service.getProdutos();
+  }
+
+  @GET
+  @Path("/tipolancamento")
+  public List<TipoLancamento> getTipoLancamentos() {
+    return service.getTiposLancamento();
   }
 
 }
