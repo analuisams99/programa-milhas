d34996e967da736533e5b75ff865580496215e99
Ana Luisa <ana.luisa.isa@hotmail.com>
2023-04-05 20:20:45 -0300

    feat(Dominio/DAO): Implementação do método getProdutos da classe DominioDao

--- src/main/java/com/trybe/acc/java/programamilhas/dao/DominioDao.java
+++ src/main/java/com/trybe/acc/java/programamilhas/dao/DominioDao.java
@@ -1,6 +1,7 @@
 package com.trybe.acc.java.programamilhas.dao;
 
 import com.trybe.acc.java.programamilhas.model.Parceiro;
+import com.trybe.acc.java.programamilhas.model.Produto;
 import java.util.List;
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
@@ -16,6 +17,11 @@ public class DominioDao {
   public List<Parceiro> getParceiros() {
     String hql = "SELECT p FROM Parceiro p";
     return entityManager.createQuery(hql, Parceiro.class).getResultList();
+  }
+
+  public List<Produto> getProdutos() {
+    String hql = "SELECT p FROM Produto p";
+    return entityManager.createQuery(hql, Produto.class).getResultList();
   }
 
 }
