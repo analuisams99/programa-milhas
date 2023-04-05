29b16739037c2cf0d91e9534cbf64b36b10ab642
Ana Luisa <ana.luisa.isa@hotmail.com>
2023-04-05 20:05:31 -0300

    feat(Admin/Resource): Implementação do método adicionaCredito da classe AdminResource, no caminho /admin, injetando as dependencias AdminService e TokenUtil

---
+++ src/main/java/com/trybe/acc/java/programamilhas/rest/AdminResource.java
@@ -0,0 +1,33 @@
+package com.trybe.acc.java.programamilhas.rest;
+
+import com.trybe.acc.java.programamilhas.dto.LancamentoDto;
+import com.trybe.acc.java.programamilhas.exception.AcessoNaoAutorizadoException;
+import com.trybe.acc.java.programamilhas.result.MensagemResult;
+import com.trybe.acc.java.programamilhas.service.AdminService;
+import com.trybe.acc.java.programamilhas.util.TokenUtil;
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.QueryParam;
+
+/**Classe AdminResource.*/
+@Path("/admin")
+@ApplicationScoped
+public class AdminResource {
+
+  @Inject
+  AdminService adminService;
+
+  @Inject
+  TokenUtil tokenUtil;
+
+  @POST
+  @Path("/credito")
+  public MensagemResult adicionaCredito(@QueryParam("token") String token, LancamentoDto lancamento)
+      throws AcessoNaoAutorizadoException {
+    tokenUtil.validarAdmToken(token);
+    return adminService.adicionaCredito(lancamento);
+  }
+
+}
