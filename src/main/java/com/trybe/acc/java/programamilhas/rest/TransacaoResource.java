3672b9694a8c0c78c2c9b3070d5c495f5139ee41
Ana Luisa <ana.luisa.isa@hotmail.com>
2023-04-05 20:44:10 -0300

    feat(Transacao/Resource): Implementação do método resgateProduto da classe TransacaoResource, injetando as dependencias TransacaoService e TokenUtil

---
+++ src/main/java/com/trybe/acc/java/programamilhas/rest/TransacaoResource.java
@@ -0,0 +1,36 @@
+package com.trybe.acc.java.programamilhas.rest;
+
+import com.trybe.acc.java.programamilhas.dto.ResgateProdutoDto;
+import com.trybe.acc.java.programamilhas.exception.AcessoNaoAutorizadoException;
+import com.trybe.acc.java.programamilhas.exception.SaldoInsuficienteException;
+import com.trybe.acc.java.programamilhas.result.MensagemResult;
+import com.trybe.acc.java.programamilhas.service.TransacaoService;
+import com.trybe.acc.java.programamilhas.util.TokenUtil;
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.QueryParam;
+
+/**Classe TransacaoResource.*/
+@Path("/transacao")
+@ApplicationScoped
+public class TransacaoResource {
+
+  @Inject
+  TransacaoService service;
+
+  @Inject
+  TokenUtil tokenUtil;
+
+  @POST
+  @Path("/resgate-produto")
+  public MensagemResult resgateProduto(
+      @QueryParam("token") String token, 
+      ResgateProdutoDto resgate
+  ) throws AcessoNaoAutorizadoException, SaldoInsuficienteException {
+    Integer idUsuario = tokenUtil.obterIdUsuario(token);
+    return service.resgateProduto(idUsuario, resgate);
+  }
+
+}
